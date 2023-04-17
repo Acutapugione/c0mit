31f52ad1100a9a97b26d84124b97789cfc38a73c
Acutapugione <acuta.pugione@gmail.com>
2023-04-17 18:09:55 +0300

    human

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# from human import Human
+from human import Human
 
 
-# def main_loop():
-#     person = Human("Володимир", 11, "lazyyyy")
-#     person.voice = voice
-#     person.voice()
-#     print(person.__dict__)
-#     for key, val in person.__dict__.items():
-#         print(f"{key} = {val}")
+def main_loop():
+    person = Human("Володимир", 11, "lazyyyy")
+    person.voice = voice
+    person.voice()
+    print(person.__dict__)
+    for key, val in person.__dict__.items():
+        print(f"{key} = {val}")
 
 
-# def voice():
-#     print("sate B")
+def voice():
+    print("sate B")
 
 
-# if __name__ == '__main__':
-#     main_loop()
+if __name__ == '__main__':
+    main_loop()
